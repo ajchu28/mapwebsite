513f46bcec54e4418e31b8d96612c78c31ff9e34
ajchu28 <alliechu@gmail.com>
2022-07-05 11:43:24 -0500

    merged stories

--- javascript/story.js
+++ javascript/story.js
@@ -14,27 +14,35 @@ var stories = [
       description: "How do we understand racial segregation? Traditionally, we think about racial segregation in terms of neighborhoods - where people live.\
       A New Picture of Segregation describes a different way of thinking about segregation.\
       We account for the other spaces and opportunities that make up our daily social exposures.",
-    //   layer: "wei_data",
-    //   flyTo: {
-    //     zoom: 0,
-    //     center: [0, 0]
-    //   },
+      layer: "blk_group",
+      flyTo: {
+        zoom: 11,
+        center: [-87.623177, 41.881832]
+      },
     },
-    { title: "Story 2",
-      description: "Text text.",
-    //   layer: "ce_data",
-    //   flyTo: {
-    //     zoom: 0,
-    //     center: [0, 0]
-    //   },
+    { title: "Beverly vs. Washington Heights",
+      description: "South Beverly Boulevard is a dividing line between the community districts of Beverly to the west and Washington Heights to the east. While Beverly is considered to be one of the more racially diverse neighborhoods in the city, Washington Heights saw substantial racial turnover during the 1970s and has been a majority Black neighborhood since, with 96.1% of area's residents identifying as Black, according to the 2015-2019 American Census Survey.",
+      layer: "blk_group",
+      flyTo: {
+        zoom: 13,
+        center: [-87.662654,41.720481]
+      },
     },
-    { title: "Story 3",
-      description: "Text text text.",
-    //   layer: "ce_data",
-    //   flyTo: {
-    //     zoom: 0,
-    //     center: [0, 0]
-    //   },
+    {
+      description: "However, when we look at the overall experienced racial exposure of people moving throughout these neighborhoods over time, we can see that Washington Heights is more diverse in its activity than what the residential diversity of the areas might suggest.",
+      layer: "ce_group",
+      flyTo: {
+        zoom: 13,
+        center: [-87.662654,41.720481]
+      },
+    },
+    {
+      description: "And when we look at the interaction exposure, which measures the diversity of the potential interactions in the areas, it seems that, actually, both neighborhoods are equally diverse in this respect. Moreover, the higher diversity areas are along 95th street, which is a major commerical street in the area. ",
+      layer: "wei_group",
+      flyTo: {
+        zoom: 13,
+        center: [-87.662654,41.720481]
+      },
     },
 ]
 
@@ -42,18 +50,32 @@ var stories = [
 
 // Update Story.
 function updateStory(storyObj) {
+
+    const blk_group = ['blkgrp_exp_results_1','blkgrp_exp_results_2','blkgrp_exp_results_3'];
+    const wei_group = ['wei_results_1', 'wei_results_2', 'wei_results_3'];
+    const ce_group = ['cell_exp_results_1','cell_exp_results_2','cell_exp_results_3'];
+
+    const layer_mapping = {
+      'wei_group': wei_group,
+      'ce_group': ce_group,
+      'blk_group': blk_group,
+  };
   
     // Story vars.
     var title = storyObj['title'];
     var description = storyObj['description'];
-    // var cameraSettings = storyObj['flyTo'];
-    // var layer = storyObj['layer'];
+    var cameraSettings = storyObj['flyTo'];
+    var layer = storyObj['layer'];
+    var array = []
 
     // Update the Storymode content.
     storyHeader.text(title);
     storyContent.text(description);
-    // map.flyTo(cameraSettings);
+    map.flyTo(cameraSettings);
+
     // call to update to active layer
+    make_visible(layer_mapping[layer], array);
+    $("#"+layer).prop("checked", true);
 };
 
 // Callbacks
